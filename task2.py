3e594912e31011e6ce84dc4691203dbc89332708
PranshuSingh3825 <pranshup053@gmail.com>
2025-09-26 17:54:06 +0530

    Update task2.py

---
+++ task2.py
@@ -0,0 +1,11 @@
+# Create a list of numbers 1 to 10
+numbers = list(range(1, 11))
+print("Original list:", numbers)
+
+# Extract the first five elements
+first_five = numbers[:5]
+print("Extracted first five elements:", first_five)
+
+# Reverse these extracted elements
+reversed_list = first_five[::-1]
+print("Reversed extracted elements:", reversed_list)
